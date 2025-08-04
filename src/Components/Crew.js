67e1e6290424c578ec98560b846c9d42997d7ca6
Sohyb01 <sohyb0155@gmail.com>
2025-08-04 14:41:03 +0200

    id

--- src/Components/Crew.js
+++ src/Components/Crew.js
@@ -1,56 +1,102 @@
-import React from 'react'
-import { useState, useEffect } from "react"
-import dataset from '../data'
+import React from "react";
+import { useState, useEffect } from "react";
+import dataset from "../data";
 
 const Crew = () => {
-  const [data, setData] = useState(dataset)
-  const [crewData, setCrewData] = useState(data.crew[0])
-  const [crewActiveLinks, setCrewActiveLinks] = useState( ['active', '', '', ''] )
+  const [data, setData] = useState(dataset);
+  const [crewData, setCrewData] = useState(data.crew[0]);
+  const [crewActiveLinks, setCrewActiveLinks] = useState([
+    "active",
+    "",
+    "",
+    "",
+  ]);
 
-  console.log(crewActiveLinks[2])
+  console.log(crewActiveLinks);
+
   return (
     <>
       <div className="container-crew">
-        <h2><span>02</span>Meet your crew</h2>
-        <img className='crew-img' src={crewData.images.png} alt="" />
+        <h2>
+          <span>02</span>Meet your crew
+        </h2>
+        <img className="crew-img" src={crewData.images.png} alt="" />
         <div className="gray-line"></div>
         <div className="crew-navigation">
-          <button className="dot-indicator" onClick={()=> setCrewData(data.crew[0])}></button>
-          <button className="dot-indicator" onClick={()=> setCrewData(data.crew[1])}></button>
-          <button className="dot-indicator" onClick={()=> setCrewData(data.crew[2])}></button>
-          <button className="dot-indicator" onClick={()=> setCrewData(data.crew[3])}></button>
+          <button
+            className={`dot-indicator ` + crewActiveLinks[0]}
+            onClick={() => {
+              setCrewData(data.crew[0]);
+              setCrewActiveLinks(["active", " ", " ", " "]);
+            }}
+          ></button>
+          <button
+            className={`dot-indicator ` + crewActiveLinks[1]}
+            onClick={() => {
+              setCrewData(data.crew[1]);
+              setCrewActiveLinks([" ", "active", " ", " "]);
+            }}
+          ></button>
+          <button
+            className={`dot-indicator ` + crewActiveLinks[2]}
+            onClick={() => {
+              setCrewData(data.crew[2]);
+              setCrewActiveLinks([" ", " ", "active", " "]);
+            }}
+          ></button>
+          <button
+            className={`dot-indicator ` + crewActiveLinks[3]}
+            onClick={() => {
+              setCrewData(data.crew[3]);
+              setCrewActiveLinks([" ", " ", " ", "active"]);
+            }}
+          ></button>
         </div>
         <div className="crew-role">{crewData.role}</div>
         <div className="crew-name">{crewData.name}</div>
         <div className="crew-bio">{crewData.bio}</div>
       </div>
       <div className="container-crew-tablet">
-        <h2><span>02</span>Meet your crew</h2>
-          <div className="crew-role">{crewData.role}</div>
-          <div className="crew-name">{crewData.name}</div>
-          <div className="crew-bio">{crewData.bio}</div>
-          <div className="crew-navigation">
-            <button className={`dot-indicator ` + crewActiveLinks[0]} onClick={()=> { 
-              setCrewData(data.crew[0])
-              setCrewActiveLinks(['active',' ',' ',' '])
-              }}></button>
-            <button className={`dot-indicator ` + crewActiveLinks[1]} onClick={()=> { 
-              setCrewData(data.crew[1])
-              setCrewActiveLinks([' ','active',' ',' '])
-              }}></button>
-            <button className={`dot-indicator ` + crewActiveLinks[2]} onClick={()=> { 
-              setCrewData(data.crew[2])
-              setCrewActiveLinks([' ',' ','active',' '])
-              }}></button>
-            <button className={`dot-indicator ` + crewActiveLinks[3]} onClick={()=> { 
-              setCrewData(data.crew[3])
-              setCrewActiveLinks([' ',' ',' ','active'])
-              }}></button>
-          </div>
-          <img className='crew-img' src={crewData.images.png} alt="" />
+        <h2>
+          <span>02</span>Meet your crew
+        </h2>
+        <div className="crew-role">{crewData.role}</div>
+        <div className="crew-name">{crewData.name}</div>
+        <div className="crew-bio">{crewData.bio}</div>
+        <div className="crew-navigation">
+          <button
+            className={`dot-indicator ` + crewActiveLinks[0]}
+            onClick={() => {
+              setCrewData(data.crew[0]);
+              setCrewActiveLinks(["active", " ", " ", " "]);
+            }}
+          ></button>
+          <button
+            className={`dot-indicator ` + crewActiveLinks[1]}
+            onClick={() => {
+              setCrewData(data.crew[1]);
+              setCrewActiveLinks([" ", "active", " ", " "]);
+            }}
+          ></button>
+          <button
+            className={`dot-indicator ` + crewActiveLinks[2]}
+            onClick={() => {
+              setCrewData(data.crew[2]);
+              setCrewActiveLinks([" ", " ", "active", " "]);
+            }}
+          ></button>
+          <button
+            className={`dot-indicator ` + crewActiveLinks[3]}
+            onClick={() => {
+              setCrewData(data.crew[3]);
+              setCrewActiveLinks([" ", " ", " ", "active"]);
+            }}
+          ></button>
+        </div>
+        <img className="crew-img" src={crewData.images.png} alt="" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Crew
+export default Crew;
